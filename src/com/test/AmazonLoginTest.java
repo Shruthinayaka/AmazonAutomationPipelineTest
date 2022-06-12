a817c5aaa61fe2ad47146a9655b18e7cb0ba8fd2
Shruthinayaka <shruthinayaka92@gmail.com>
2022-06-12 19:21:16 +0530

    commiting pipelne changes

---
+++ src/com/test/AmazonLoginTest.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.testng.annotations.Test;
+
+public class AmazonLoginTest {
+
+	@Test
+	
+	public void LoginApplication()
+	{
+		System.out.println("Login Test Case");
+	}
+}
